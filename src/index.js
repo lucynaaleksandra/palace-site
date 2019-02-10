80e724102b612517d9291dfbdf4867a1beca9069
Lucyna Aleksandra <lucyaleksa@gmail.com>
2019-02-10 11:17:14 -0500

    setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import "./index.scss";
+
+// LAZY LOADING
+document.addEventListener("DOMContentLoaded", function() {
+  var sections = [].slice.call(document.querySelectorAll(".la-animatable"));
+
+  if ("IntersectionObserver" in window) {
+    let sectionObserver = new IntersectionObserver(
+      function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("la-animatable--animate");
+            sectionObserver.unobserve(entry.target);
+          }
+          // else {
+          //   entry.target.classList.remove("la-animatable--animate")
+          // }
+        });
+      },
+      {
+        rootMargin: "0px 0px -500px 0px"
+      }
+    );
+
+    sections.forEach(function(section) {
+      sectionObserver.observe(section);
+    });
+  }
+});
